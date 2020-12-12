7eeb52c82f01074842e3207b045a4dc7ad10f7cd
Idan Amati <idan.amati@gmail.com>
2020-12-12 16:58:41 +0200

    hero css fix

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,17 +2,17 @@ import Button from './base/Button';
 
 export default function Hero({ img, title, text, action, to }) {
   return (
-    <div className="max-h-80v overflow-hidden relative">
-      <div className="absolute w-full h-full flex items-center pt-20">
+    <div className="h-80v overflow-hidden relative">
+      <div className="absolute w-full h-full flex pb-5 items-end px-3 md:items-center md:pt-20">
         <div className="container mx-auto text-white">
-          <h1 className="text-9xl">{title}</h1>
-          <h2 className="text-5xl mb-3">{text}</h2>
+          <h1 className="text-9xl text-shadow">{title}</h1>
+          <h2 className="text-5xl mb-3 text-shadow">{text}</h2>
           <Button link={to} white>
             {action}
           </Button>
         </div>
       </div>
-      <img src={img} alt={text} className="min-h-96 object-cover w-full" />
+      <img src={img} alt={text} className="h-full object-cover w-full" />
     </div>
   );
 }
