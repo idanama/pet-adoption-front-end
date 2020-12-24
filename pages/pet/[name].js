225355f9ff099e1ac0af230b75eb5cf53d58da04
Idan Amati <idan.amati@gmail.com>
2020-12-24 20:16:41 +0200

    api integration

--- pages/pet/[name].js
+++ pages/pet/[name].js
@@ -15,6 +15,8 @@ import {
 import { IconContext } from 'react-icons';
 import { formatDistanceToNow } from 'date-fns';
 import Button from '../../components/base/Button';
+import Register from '../../components/Register';
+import Login from '../../components/Login';
 import Modal from '../../components/base/Modal';
 import api from '../../utils/api';
 
@@ -23,7 +25,7 @@ import userContext from '../../context/userContext';
 export default function PetPage() {
   const { name } = useRouter().query;
 
-  const { user } = useContext(userContext);
+  const { user, loggedIn } = useContext(userContext);
 
   const [pet, setPet] = useState({
     Name: 'Loading', pictures: [''], gender: '', dateOfBirth: new Date(), type: '',
@@ -37,8 +39,10 @@ export default function PetPage() {
   const fetchPetByName = async (petName) => {
     setLoading(true);
     const petResponse = await api.getPetByName(petName);
-    // const userOwned = pets[petResponse.id] !== undefined;
-    const userOwned = false;
+    let userOwned = false;
+    if (user._id) {
+      userOwned = petResponse.owner === user._id;
+    }
     setPet({ ...petResponse, userOwned });
     setPetTable([
       { title: 'Gender', value: petResponse.gender, icon: petResponse.gender === 'Male' ? <RiMenLine /> : <RiWomenLine /> },
@@ -53,14 +57,47 @@ export default function PetPage() {
     setLoading(false);
   };
 
-  const adoptPet = async () => {
-    await fetch('/api/pet/adopt', { method: 'POST', body: await JSON.stringify({ petId: pet.id, userId: user.id }) });
+  const changePetOwnership = async (status) => {
+    let petResponse;
+    switch (status) {
+      case 'adopt':
+        petResponse = await api.adoptPet(user._id, pet._id, 'adopt');
+        break;
+      case 'foster':
+        petResponse = await api.adoptPet(user._id, pet._id, 'foster');
+        break;
+      case 'return':
+        petResponse = await api.returnPet(user._id, pet._id);
+        break;
+      default:
+        break;
+    }
+    setPet(petResponse);
   };
 
-  const changePetStatus = () => {
-    adoptPet();
+  const likePet = async () => {
+    setSaved(true);
+    const { savedPets, ok } = await api.savePet(user._id, pet._id);
+    if (!ok || !savedPets.includes(pet._id)) {
+      setSaved(false);
+    }
   };
 
+  const unlikePet = async () => {
+    setSaved(false);
+    const { savedPets, ok } = await api.deleteSavedPet(user._id, pet._id);
+    if (!ok || savedPets.includes(pet._id)) {
+      setSaved(true);
+    }
+  };
+
+  const toggleLikePet = () => {
+    if (saved) {
+      unlikePet();
+    } else {
+      likePet();
+    }
+  };
   useEffect(() => {
     if (name !== undefined) {
       fetchPetByName(name);
@@ -74,7 +111,7 @@ export default function PetPage() {
         <div className="w-2/3 m-4">
           <div className="text-4xl pb-3 flex justify-between items-center">
             <h1>{pet.name}</h1>
-            <Button transparent className="flex flex-col items-center w-5" onClick={() => setSaved(!saved)}>
+            <Button transparent className="flex flex-col items-center w-5" onClick={() => toggleLikePet()}>
               {!saved && (
               <>
                 <RiHeartLine size="0.8em" />
@@ -119,35 +156,52 @@ export default function PetPage() {
             {pet.bio}
           </div>
         </div>
-        <div className="md:px-5 w-1/3 py-5">
-          <div className="sticky top-0 p-5 border rounded-xl shadow-xl">
-            <div className="grid grid-cols-1 gap-2">
-              { !pet.userOwned
-                && (
+        <div className="md:px-5 w-1/3">
+          <div className="sticky top-1/3 py-5">
+            <div className=" p-5 border rounded-xl shadow-xl grid grid-cols-1 gap-2 min-h-">
+              { !loading && pet.owner === null && (
                 <>
                   <div className="text-xl">
                     {`Want to have ${pet.name}?`}
                   </div>
-                  <Button xl primary onClick={() => adoptPet('adopt')}>Adopt</Button>
-                  <Button white onClick={() => changePetStatus('foster')}>Foster</Button>
+                    { loggedIn && (
+                    <>
+                      <Button xl primary onClick={() => changePetOwnership('adopt')}>Adopt</Button>
+                      <Button white onClick={() => changePetOwnership('foster')}>Foster</Button>
+                    </>
+                    )}
+                    { !loggedIn && (
+                    <div className="grid grid-cols-2 gap-2">
+                      <Button white onClick={() => setModal('register')}>Register</Button>
+                      <Button white onClick={() => setModal('login')}>Login</Button>
+                    </div>
+                    )}
                 </>
-                )}
-              {pet.userOwned && (
+              )}
+              {!loading && pet.owner === user._id && (
                 <>
                   <div className="text-xl">{`You already have ${pet.name}, isn't ${pet.gender === 'Male' ? 'he' : 'she'} cute? 😍`}</div>
                   <div>Help up save more animals!</div>
-                  <Button primary className="mb-12" link="\donate">Donate to spca</Button>
-                  <Button text className="text-gray-700" onClick={() => setModal('return')}>{`I'm a heartless person and I want to return ${pet.name}.`}</Button>
+                  {pet.status === 'Fostered' && (
+                    <>
+                      <Button primary xl onClick={() => changePetOwnership('adopt')}>
+                        Adopt
+                        {' '}
+                        { pet.name}
+                      </Button>
+                    </>
+                  )}
+                  <Button primary={pet.status === 'Adopted'} link="\donate">Donate to spca</Button>
+                  <Button text className="text-gray-700" onClick={() => setModal('return')}>{`Things are not going well between me and ${pet.name}.`}</Button>
                   {modal === 'return' && (
                     <Modal title="Return a pet" close={() => setModal('')}>
                       <div className="grid grid-col-1 gap-3">
                         <div>
-                          Pets have feelings and get traumatized when they become orphaned
-                          willingly, if you have any issues please ask for advice before giving up.
-
+                          Pets just want to be loved, sometimes we don&apos;t understand each other.
+                          If you have any issues please ask for advice before giving up.
                         </div>
                         <Button xl primary onClick={() => setModal('')}>{`I will be a good parent to ${pet.name}`}</Button>
-                        <Button text onClick={() => { changePetStatus(undefined); setModal(''); }}>{`Return ${pet.name}`}</Button>
+                        <Button text onClick={() => { changePetOwnership('return'); setModal(''); }}>{`Return ${pet.name}`}</Button>
                         {' '}
                       </div>
                     </Modal>
@@ -158,6 +212,8 @@ export default function PetPage() {
           </div>
         </div>
       </div>
+      {modal === 'register' && (<Register close={() => setModal('')} />)}
+      {modal === 'login' && (<Login close={() => setModal('')} />)}
     </div>
   );
 }
