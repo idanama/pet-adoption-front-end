c5c73cba5b270f9bbe98b04545b09cf797f3b4a3
Idan Amati <idan.amati@gmail.com>
2020-12-24 16:02:03 +0200

    simplified hero data logic

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,15 +1,17 @@
 import Button from './base/Button';
 
-export default function Hero({ img, title, text, action, to }) {
+export default function Hero({ img = '', title = '', text = '', action, to }) {
   return (
     <div className="h-80v overflow-hidden relative">
       <div className="absolute w-full h-full flex pb-5 items-end px-3 md:items-center md:pt-20">
         <div className="container mx-auto text-white">
           <h1 className="text-9xl text-shadow">{title}</h1>
           <h2 className="text-5xl mb-3 text-shadow">{text}</h2>
-          <Button link={to.toLowerCase()} white>
-            {action}
-          </Button>
+          {action && (
+            <Button link={to.toLowerCase()} white>
+              {action}
+            </Button>
+          )}
         </div>
       </div>
       <img
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,27 @@ import Hero from '../components/Hero';
 import api from '../utils/api';
 
 export default function Home() {
-  const [heroPet, setHeroPet] = useState({ Name: 'SPCA', tagline: 'Society for Prevention of Cruelty to Animals in Israel', pictures: [''] });
+  const [heroPet, setHeroPet] = useState(
+    {
+      img: '',
+      title: 'SPCA',
+      text: 'Society for Prevention of Cruelty to Animals in Israel',
+      action: ``,
+      to: `/`,
+    }
+  );
 
   const fetchRandomPet = async () => {
-    const petResponse = await api.getRandomPet();
-    setHeroPet(petResponse);
+    const pet = await api.getRandomPet();
+    if (pet.ok) {
+      setHeroPet({
+        img: pet.pictures[0],
+        title: pet.name,
+        text: pet.tagline,
+        action:`Meet ${pet.name}`,
+        to: `/pet/${pet.name}`,
+      });
+    }
   };
 
   useEffect(() => {
@@ -23,7 +39,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Hero img={`${heroPet.pictures[0]}`} title={heroPet.name} text={heroPet.tagline} action={`Meet ${heroPet.name}`} to={`/pet/${heroPet.name}`} />
+        <Hero img={heroPet.img} title={heroPet.title} text={heroPet.text} action={heroPet.action} to={heroPet.to} />
       </main>
     </div>
   );
