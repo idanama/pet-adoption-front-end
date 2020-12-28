c61f4a396665cf1ceac3f9aa11234b5ce3006cc2
Idan Amati <idan.amati@gmail.com>
2020-12-28 15:46:19 +0200

    added search results page

---
+++ pages/search.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import api from '../utils/api';
+import PetCard from '../components/PetCard';
+
+export default function Search() {
+  const [petResults, setPetResults] = useState([]);
+
+  const { query } = useRouter();
+  let { animal, relationship } = query;
+  if (relationship === 'any' || !relationship) {
+    relationship = 'adopt or foster';
+  }
+  if (animal === 'any' || !animal) {
+    animal = 'pet';
+  }
+
+  const updateResults = async (queryParams) => {
+    try {
+      const urlParams = new URLSearchParams();
+      Object.keys(queryParams).forEach((key) => {
+        urlParams.append(key, queryParams[key]);
+      });
+      const { res } = await api.getPets(`?${urlParams.toString()}`);
+      setPetResults(res);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    updateResults(query);
+  }, [query]);
+
+  return (
+    <>
+      <div className="container mx-auto max-w-screen-lg px-3 mt-40">
+        <h1 className="text-3xl">
+          <span className="capitalize">{`${animal}s to ${relationship}`}</span>
+        </h1>
+        <section className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {petResults.map((pet) => (
+            <PetCard key={`saved-${pet._id}`} pet={pet} />
+          ))}
+        </section>
+      </div>
+    </>
+  );
+}
