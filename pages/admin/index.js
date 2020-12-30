1773acfb69b01ddc567c263e79436b2bb02082c1
Idan Amati <idan.amati@gmail.com>
2020-12-30 14:50:28 +0200

    dashboard pages init

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,33 +1,68 @@
-import { useEffect, useContext, useState } from 'react';
-import { useRouter } from 'next/router';
-import Button from '../../components/base/Button';
+import { useContext } from 'react';
+import Link from 'next/link';
+
+import {
+  RiArrowRightSLine, RiFolderShieldLine, RiFolderUserLine, RiHeartAddLine,
+} from 'react-icons/ri';
 
 import userContext from '../../context/userContext';
 
 export default function Dashboard() {
-  const router = useRouter();
-  const { user, loading } = useContext(userContext);
-  const [isAdmin, setIsAdmin] = useState(false);
+  const { user } = useContext(userContext);
 
-  useEffect(() => {
-    if (!loading) {
-      if (user.role !== 'admin') {
-        router.push('/');
-      } else {
-        setIsAdmin(true);
-      }
-    }
-  }, [user, loading, router]);
+  const actions = [
+    {
+      name: 'Add a Pet',
+      text: 'Put up a new pet adoption for adoption.',
+      icon: <RiHeartAddLine />,
+      href: '/admin/pets/add',
+    },
+    {
+      name: 'Manage Users',
+      text: 'See recent adoptions, users and contact users.',
+      icon: <RiFolderUserLine />,
+      href: '/admin/users/manage',
+    },
+    {
+      name: 'Manage Pets',
+      text: 'Edit pet pages and information.',
+      icon: <RiFolderShieldLine />,
+      href: '/admin/pets/manage',
 
-  if (isAdmin) {
+    },
+  ];
+
+  if (user.role === 'admin') {
     return (
-      <div className="container mx-auto max-w-screen-lg px-7 lg:px-1">
+      <div className="container-min">
         <h1 className="text-3xl mb-7">Dashboard</h1>
-        <ul>
-          <li><Button>Add Pet</Button></li>
-          <li>Manage Users</li>
-          <li>Manage Pets</li>
-        </ul>
+        <h3 className="text-xl">
+          <span className="font-bold">
+            {`${user.fName} ${user.lName}`}
+          </span>
+          {' · '}
+          <span className="text-green-500">
+            <Link href="/profile">Go to profile</Link>
+          </span>
+        </h3>
+        <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-max gap-4">
+          {actions.map((action) => (
+            <Link href={action.href} key={action.name}>
+              <a>
+                <div className="p-6 shadow-md w-72 h-44">
+                  <div className="text-3xl mb-4">
+                    {action.icon}
+                  </div>
+                  <div className="text-xl flex items-center">
+                    {action.name}
+                    <RiArrowRightSLine />
+                  </div>
+                  <div>{action.text}</div>
+                </div>
+              </a>
+            </Link>
+          ))}
+        </div>
       </div>
     );
   }
---
+++ pages/admin/pets/add.jsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import Link from 'next/link';
+
+import userContext from '../../../context/userContext';
+
+export default function AddPet() {
+  const { user } = useContext(userContext);
+
+  if (user.role === 'admin') {
+    return (
+      <div className="container-max">
+        <h3 className="text-xl mb-4">
+          <span className="text-green-500">
+            <Link href="/admin">← Dashboard</Link>
+          </span>
+        </h3>
+        <h1 className="text-3xl mb-7">Add a Pet</h1>
+      </div>
+    );
+  }
+  return <></>;
+}
