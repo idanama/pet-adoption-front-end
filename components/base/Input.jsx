506615955bfd8300bfe788d11e53dbcc00726d69
Idan Amati <idan.amati@gmail.com>
2021-01-05 15:38:18 +0200

    display error prop

--- components/base/Input.jsx
+++ components/base/Input.jsx
@@ -19,65 +19,79 @@ export default function Input({
     !label || labelPlaceholder ? 'py-3' : 'pt-5 pb-1';
 
   return (
-    <label htmlFor={name} className="relative mb-4 flex w-full">
-      {label && (
-        <div
-          className={`absolute left-3 duration-100 z-10 ${
-            labelPlaceholder
-              ? 'top-1/2 transform -translate-y-1/2'
-              : 'text-sm top-1'
-          }`}
-        >
-          {label}
+    <div className="mb-4 w-full">
+      <label htmlFor={name} className="relative flex w-full">
+        {label && (
+          <div
+            className={`absolute left-3 duration-100 z-10 ${
+              labelPlaceholder
+                ? 'top-1/2 transform -translate-y-1/2'
+                : 'text-sm top-1'
+            }`}
+          >
+            {label}
+          </div>
+        )}
+        {type !== 'textarea' && (
+          <input
+            required={required}
+            type={type}
+            name={name}
+            id={name}
+            value={
+              type !== 'date'
+                ? value
+                : value && new Date(value).toISOString().replace(/T.*/, '')
+            }
+            placeholder={placeholder}
+            onFocus={() => {
+              setFocus(true);
+            }}
+            onBlur={() => {
+              setFocus(false);
+            }}
+            onChange={(e) => {
+              onChange({ [e.target.name]: e.target.value });
+            }}
+            min={0}
+            className={`border px-3
+            ${error ? 'border-error' : ''}
+            ${className} transition-all duration-100 ${placeholderInputPadding} w-full rounded-md focus:border-black focus:border-2`}
+          />
+        )}
+        {suffix && (
+          <span className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-500">
+            {suffix}
+          </span>
+        )}
+        {type === 'textarea' && (
+          <textarea
+            className={`border 
+              ${error ? 'border-error' : ''}
+              px-3 ${className} ${placeholderInputPadding} w-full rounded-md focus:border-black focus:border-2`}
+            name={name}
+            id={name}
+            value={value}
+            placeholder={placeholder}
+            cols="30"
+            rows="3"
+            onFocus={() => {
+              setFocus(true);
+            }}
+            onBlur={() => {
+              setFocus(false);
+            }}
+            onChange={(e) => {
+              onChange({ [e.target.name]: e.target.value });
+            }}
+          />
+        )}
+      </label>
+      {error && (
+        <div className="text-base font-normal font-sans ml-3 mt-1 text-error">
+          {error}
         </div>
       )}
-      {type !== 'textarea' && (
-        <input
-          required={required}
-          type={type}
-          name={name}
-          id={name}
-          value={value}
-          placeholder={placeholder}
-          onFocus={() => {
-            setFocus(true);
-          }}
-          onBlur={() => {
-            setFocus(false);
-          }}
-          onChange={(e) => {
-            onChange({ [e.target.name]: e.target.value });
-          }}
-          min={0}
-          className={`border px-3 ${className} transition-all duration-100 ${placeholderInputPadding} w-full rounded-md focus:border-black focus:border-2`}
-        />
-      )}
-      {suffix && (
-        <span className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-500">
-          {suffix}
-        </span>
-      )}
-      {type === 'textarea' && (
-        <textarea
-          className={`border px-3 ${className} ${placeholderInputPadding} w-full rounded-md focus:border-black focus:border-2`}
-          name={name}
-          id={name}
-          value={value}
-          placeholder={placeholder}
-          cols="30"
-          rows="3"
-          onFocus={() => {
-            setFocus(true);
-          }}
-          onBlur={() => {
-            setFocus(false);
-          }}
-          onChange={(e) => {
-            onChange({ [e.target.name]: e.target.value });
-          }}
-        />
-      )}
-      {error && <div className="ml-3 mt-1 text-red-700">{error}</div>}
-    </label>
+    </div>
   );
 }
--- components/base/Options.jsx
+++ components/base/Options.jsx
@@ -6,6 +6,7 @@ export default function Options({
   children,
   fullwidth,
   className,
+  error,
 }) {
   const [open, setOpen] = useState(false);
   const selectorRef = useRef(null);
@@ -13,42 +14,45 @@ export default function Options({
   const selectionButtonStyle = {
     all: `relative bg-white rounded-full cursor-pointer`,
     open: 'border-black',
-    regular: 'border py-3 px-3 ',
+    regular: `border py-3 px-3 ${error ? 'border-error' : ''}`,
   };
 
   const openButtonStyle =
     'bg-white rounded-3xl py-1 border border-black border-2 mt-2 overflow-hidden ';
 
   return (
-    <div
-      className={`${selectionButtonStyle.all} ${className}
+    <div className={fullwidth && 'w-full'}>
+      <div
+        className={`${selectionButtonStyle.all} ${className}
         ${open ? selectionButtonStyle.open : 'hover:bg-gray-100'}
         ${selectionButtonStyle.regular}
-        ${fullwidth && 'w-full'}`}
-      onClick={() => {
-        setOpen(!open);
-      }}
-      tabIndex="0"
-      onBlur={(e) => {
-        console.log('here');
-        setTimeout(() => {
-          setOpen(false);
-        }, 100);
-      }}
-      onBlur={() => setOpen(false)}
-      ref={selectorRef}
-    >
-      {value === '' && <div className="text-gray-500">{label}</div>}
-      {value !== '' && <div className="capitalize">{value}</div>}
-      <div
-        className={`${
-          !open && 'hidden'
-        } absolute left-0 -bottom-0 transform translate-y-full z-10 w-min`}
+        `}
+        onClick={() => {
+          setOpen(!open);
+        }}
+        tabIndex="0"
+        onBlur={(e) => {
+          console.log('here');
+          setTimeout(() => {
+            setOpen(false);
+          }, 100);
+        }}
+        onBlur={() => setOpen(false)}
+        ref={selectorRef}
       >
-        <div className={openButtonStyle} onClick={() => setOpen(false)}>
-          {children}
+        {value === '' && <div className="text-gray-500">{label}</div>}
+        {value !== '' && <div className="capitalize">{value}</div>}
+        <div
+          className={`${
+            !open && 'hidden'
+          } absolute left-0 -bottom-0 transform translate-y-full z-10 w-min`}
+        >
+          <div className={openButtonStyle} onClick={() => setOpen(false)}>
+            {children}
+          </div>
         </div>
       </div>
+      {error && <div className="ml-3 mt-1 text-error">{error}</div>}
     </div>
   );
 }
