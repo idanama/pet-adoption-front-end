c7216ca335bf0a5328e6d26baf6534182913e2aa
Idan Amati <idan.amati@gmail.com>
2021-01-05 20:55:37 +0200

    updated profile page

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,61 +1,120 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+
+import api from '../utils/api';
 
 import Button from '../components/base/Button';
 import Input from '../components/base/Input';
 import userContext from '../context/userContext';
 
 export default function Profile() {
-  const { user, updateUser } = useContext(userContext);
-  const [formUser, updateFormUser] = useState(user);
+  const { user } = useContext(userContext);
+
+  const [formUser, updateFormUser] = useState({
+    lName: '',
+    fName: '',
+    phone: '',
+    email: '',
+    bio: '',
+  });
+  const [loading, setLoading] = useState(true);
+  const [errors, setErrors] = useState({});
+
+  const handleEdit = (edited) => {
+    updateFormUser({ ...formUser, ...edited });
+  };
+
+  const getUser = async (userId) => {
+    setLoading(true);
+    const { res, error } = await api.getUserFull(userId);
+    if (!error) {
+      updateFormUser({ ...formUser, ...res });
+    } else {
+      setErrors({ page: 'Could not load profile ☹' });
+    }
+    setLoading(false);
+  };
+
+  const saveChanges = async () => {
+    const userToUpdate = {};
+    Object.keys(formUser).forEach((key) => {
+      if (formUser[key]) {
+        userToUpdate[key] = formUser[key];
+      }
+    });
+    const { res, error } = await api.updateUser(user._id, userToUpdate);
+    if (!error) {
+      setErrors({});
+      updateFormUser({ ...formUser, res });
+    } else {
+      setErrors(error.errors);
+    }
+  };
+
+  useEffect(() => {
+    if (user._id) {
+      getUser(user._id);
+    }
+  }, [user]);
+
+  if (errors?.page) {
+    return (
+      <div className="container-max">
+        <h1 className="text-3xl">Profile</h1>
+        <p>{errors.page}</p>
+      </div>
+    );
+  }
 
   return (
     <div className="container-max">
       <h1 className="text-3xl">Profile</h1>
-      <form className="flex pt-10 flex-col max-w-lg" autoComplete="off">
+      <form
+        action={`/api/update/${user._id}`}
+        method="put"
+        className="flex pt-10 flex-col max-w-lg"
+        autoComplete="off"
+      >
         <Input
+          label="First Name"
+          name="fName"
           value={formUser.fName}
           type="text"
-          onChange={(val) => updateFormUser({ ...formUser, fName: val })}
-          name="First Name"
+          onChange={handleEdit}
+          error={errors.fName?.message}
         />
         <Input
-          name="Last Name"
+          label="Last Name"
+          name="lName"
           value={formUser.lName}
           type="text"
-          onChange={(val) => updateFormUser({ ...formUser, lName: val })}
+          onChange={handleEdit}
+          error={errors.lName?.message}
         />
         <Input
-          name="Phone Number"
+          label="Phone Number"
+          name="phone"
           value={formUser.phone}
           type="email"
-          onChange={(val) => updateFormUser({ ...formUser, phone: val })}
+          onChange={handleEdit}
+          error={errors.phone?.message}
         />
         <Input
-          name="Email"
+          label="Email"
+          name="email"
           value={formUser.email}
           type="email"
-          onChange={(val) => updateFormUser({ ...formUser, email: val })}
+          onChange={handleEdit}
+          error={errors.email?.message}
         />
         <Input
-          name="Password"
-          value={formUser.password}
-          type="password"
-          onChange={(val) => updateFormUser({ ...formUser, password: val })}
-        />
-        <Input
-          name="Bio"
+          label="Bio"
+          name="bio"
           value={formUser.bio}
           type="textarea"
-          onChange={(val) => updateFormUser({ ...formUser, bio: val })}
+          onChange={handleEdit}
+          error={errors.bio?.message}
         />
-        <Button
-          submit
-          xl
-          className="max-w-min"
-          onClick={() => {
-            updateUser(formUser);
-          }}
-        >
+        <Button submit xl className="max-w-min" onClick={saveChanges}>
           Save
         </Button>
       </form>
