2dbdd54d8836421099211fe81a9ab3b256f0b6f7
Idan Amati <idan.amati@gmail.com>
2021-01-09 19:58:16 +0200

    improved footer menus

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,26 +1,66 @@
 import Link from 'next/link';
 
-export default function Footer() {
+export default function Footer({ admin, signedIn }) {
   return (
     <div className="border-t mt-16">
       <footer className="mt-10 mb-20 container mx-auto px-4">
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/search?animal=cat">
-              <a>Cats</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/search?animal=dog">
-              <a>Dogs</a>
-            </Link>
-          </li>
-        </ul>
+        <div className="flex flex-wrap">
+          <ul className="w-40">
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/search?animal=cat">
+                <a>Cats</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/search?animal=dog">
+                <a>Dogs</a>
+              </Link>
+            </li>
+          </ul>
+          {signedIn && (
+            <ul className="w-40">
+              <li>
+                <Link href="/profile">
+                  <a>Profile</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/mypets">
+                  <a>My Pets</a>
+                </Link>
+              </li>
+            </ul>
+          )}
+          {admin && (
+            <ul className="w-40">
+              <li>
+                <Link href="/profile">
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/admin/pets/add">
+                  <a>Add a Pet</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/admin/users/manage">
+                  <a>Manage Users</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/admin/pets/manage">
+                  <a>Manage Pets</a>
+                </Link>
+              </li>
+            </ul>
+          )}
+        </div>
         <div className="mt-12 pt-3 border-t text-center">
           SPCA
           {` - ${new Date().getFullYear()} - `}
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,13 +1,15 @@
-import { useRouter } from 'next/router';
+import { useContext } from 'react';
 import Footer from './Footer';
 import NavBar from './NavBar';
+import userContext from '../context/userContext';
 
 export default function Layout({ children }) {
+  const { user } = useContext(userContext);
   return (
     <>
       <NavBar />
       <div className="min-h-screen">{children}</div>
-      <Footer />
+      <Footer admin={user.role === 'admin'} signedIn={!!user._id} />
     </>
   );
 }
