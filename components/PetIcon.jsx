2aa5e61682d1483a81f0d98554ac99282947092d
Idan Amati <idan.amati@gmail.com>
2021-01-09 18:39:40 +0200

    pet icon square mode

--- components/PetIcon.jsx
+++ components/PetIcon.jsx
@@ -1,23 +1,21 @@
-export default function PetIcon({ photo, status, name }) {
+export default function PetIcon({ photo, status, name, circle = true, small }) {
   const photoTransformation =
-    typeof photo === 'string'
-      ? photo.replace('/upload/', '/upload/t_media_lib_thumb/')
-      : photo;
+    typeof photo === 'string' ? photo.replace('/upload/', '/upload/t_media_lib_thumb/') : photo;
+
+  const size = !small ? 'h-16 w-16' : 'h-8 w-8';
 
   return (
-    <div className="relative h-16 w-16 m-2">
-      <div className="rounded-full overflow-hidden h-16 w-16">
-        <img
-          src={photoTransformation}
-          alt={name}
-          className="object-cover h-full"
-        />
+    <div className={`relative ${size} m-2`}>
+      <div className={`${circle ? 'rounded-full' : 'rounded'} overflow-hidden ${size}`}>
+        <img src={photoTransformation} alt={name} className="object-cover h-full" />
       </div>
-      <div
-        className={`absolute bottom-1 right-1 w-3 h-3 rounded-full ${
-          status !== 'Adopted' && `bg-${status.toLowerCase()} shadow-inner`
-        }`}
-      />
+      {!small && (
+        <div
+          className={`absolute bottom-1 right-1 w-3 h-3 rounded-full ${
+            status !== 'Adopted' && `bg-${status.toLowerCase()} shadow-inner`
+          }`}
+        />
+      )}
     </div>
   );
 }
